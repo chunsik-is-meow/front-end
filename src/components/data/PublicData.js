5b13f42fa5e1c3507b14f193b4a535bea069bcd2
gmldus0582 <gmldus0582@naver.com>
2021-08-09 14:15:17 +0900

    feature:Change publicdata table

---
+++ src/components/data/PublicData.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    name: '전력데이터',
+    desc: 'fabcar',
+    download: '1209',
+    org: '공공데이터센터',
+    date: '11.12.01'
+  },
+  {
+    name: '태양열 사용',
+    desc: 'fabcar',
+    download: '1209',
+    org: '공공데이터센터',
+    date: '11.12.01'
+  },
+  {
+    name: '자판기 개수',
+    desc: 'basic',
+    download: '1209',
+    org: '한국 자판기 센터',
+    date: '11.12.01'
+  },
+  {
+    name: '코로나 확진자 분포도',
+    desc: 'basic',
+    download: '1209',
+    org: '질병청',
+    date: '11.12.01'
+  },
+  {
+    name: '부산 금정구 미세먼지 측정',
+    desc: 'basic',
+    download: '1209',
+    org: '공공데이터센터',
+    date: '11.12.01'
+  },
+  {
+    name: '부산 금정구 평균 기온',
+    desc: 'fabcar',
+    download: '1209',
+    org: '공공데이터센터',
+    date: '11.12.01'
+  }
+];
